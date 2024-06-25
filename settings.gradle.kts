86722ef8b0498c8d3d5e571e27dac924876154be
wangqiang <415906626@qq.com>
2024-06-25 18:20:25 +0800

    添加module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Utils"
 include(":app")
- 
+include(":utilsLbrary")
---
+++ utilsLbrary/src/androidTest/java/hz/wq/utilslbrary/log/LogUtils.kt
@@ -0,0 +1,11 @@
+package hz.wq.utilslbrary.log
+
+/**
+ * 描述: 日志工具类
+ */
+fun String.wqLog(){
+
+}
+
+class LogUtils {
+}
