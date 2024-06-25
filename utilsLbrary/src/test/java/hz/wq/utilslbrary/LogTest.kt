7a528b699f6bdba12ec8914648df05e95d52c3c1
wangqiang <415906626@qq.com>
2024-06-26 00:49:40 +0800

    去掉报错的导入

--- utilsLbrary/src/test/java/hz/wq/utilslbrary/LogTest.kt
+++ utilsLbrary/src/test/java/hz/wq/utilslbrary/LogTest.kt
@@ -1,19 +1,10 @@
 package hz.wq.utilslbrary
 
-import com.orhanobut.logger.AndroidLogAdapter
-import com.orhanobut.logger.FormatStrategy
-import com.orhanobut.logger.Logger
-import com.orhanobut.logger.PrettyFormatStrategy
 import hz.wq.utilslbrary.log.LogUtils
 import hz.wq.utilslbrary.log.wqLog
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
-import org.junit.Assert.*
-import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
