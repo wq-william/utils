86bbdb4d9da9a89000d0003721ead3e0379bcbe1
wangqiang <415906626@qq.com>
2024-06-26 18:05:09 +0800

    提交log 的util

--- utilsLbrary/build.gradle.kts
+++ utilsLbrary/build.gradle.kts
@@ -1,8 +1,49 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    `maven-publish`
 }
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "io.github.wq-william"
+            artifactId = "utils"
+            version = "0.0.1"
+            afterEvaluate {
+                from(components["release"])
+            }
+            pom {
+                name = "wq utils Library"
+                description = "A concise description of my library"
+                url = "https://github.com/wq-william/utils"
+                properties = mapOf(
+                    "myProp" to "value",
+                    "prop.with.dots" to "anotherValue"
+                )
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "github_13477226"
+                        name = "415906626@qq.com"
+                        email = "415906626@qq.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/wq-william/utils.git"
+                    developerConnection = "scm:git:ssh://github.com/wq-william/utils.git"
+                    url = "https://github.com/wq-william/utils"
+                }
+            }
+        }
 
+    }
+
+}
 android {
     namespace = "hz.wq.utilslbrary"
     compileSdk = 34
@@ -13,7 +54,17 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
-
+    publishing {
+        multipleVariants {
+            withSourcesJar()
+            allVariants()
+            withJavadocJar()
+        }
+//        singleVariant("release") {
+//            withSourcesJar()
+//            withJavadocJar()
+//        }
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
--- utilsLbrary/src/main/java/hz/wq/utilslbrary/log/LogUtils.kt
+++ utilsLbrary/src/main/java/hz/wq/utilslbrary/log/LogUtils.kt
@@ -37,4 +37,5 @@ object LogUtils {
     }
 
 
+
 }
